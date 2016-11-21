e24cde36b3f717d7e353e2d5999bf23d094f16d2
ftumj259 <2013470259@ftumj.ac.id>
2016-11-21 15:53:25 +0700

    Product knowledge schema description

--- db/migrate/20161121083252_create_product_knowledges.rb
+++ db/migrate/20161121083252_create_product_knowledges.rb
@@ -2,7 +2,7 @@ class CreateProductKnowledges < ActiveRecord::Migration[5.0]
   def change
     create_table :product_knowledges do |t|
       t.string :name, null: false
-      t.text :text, null: true
+      t.text :description, null: true
       t.string :file_name, null: false
 
       t.datetime :deleted_at
