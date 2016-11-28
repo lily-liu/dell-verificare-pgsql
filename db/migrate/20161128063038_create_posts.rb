8f78a41f22813d59b17806907f499c88519a31a7
wisely <wisely.dennis@student.sgu.ac.id>
2016-11-28 13:36:44 +0700

    migration and model stub for post

---
+++ db/migrate/20161128063038_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: true
+      t.string :content, null: false
+      t.belongs_to :user, index: true, foreign_key: true
+      t.integer :type, null: false
+      t.integer :parent_id, null: true
+
+      t.datetime :deleted_at, null: true
+      t.timestamps
+    end
+    add_foreign_key :posts, :posts, column: :parent_id
+  end
+end
