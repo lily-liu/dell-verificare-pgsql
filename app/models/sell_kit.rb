3b7fbca7a799ce8be54a79c9d05262ad2355364f
wisely <wisely.dennis@student.sgu.ac.id>
2016-12-05 22:22:16 +0700

    fixed conflicted enum

--- app/models/sell_kit.rb
+++ app/models/sell_kit.rb
@@ -1,6 +1,6 @@
 class SellKit < ApplicationRecord
-  enum category: [:other, :laptop, :desktop, :monitor, :program_info]
-  enum family: [:other_family, :inspiron_3000, :inspiron_5000, :inspiron_7000, :vostro, :xps_12, :xps_13, :xps_15, :inspiron_desktop, :inspiron_aio, :monitor, :promotion]
+  enum category: [:others, :laptops, :desktops, :monitors, :program_infos]
+  enum family: [:other_family, :inspiron_3000, :inspiron_5000, :inspiron_7000, :vostro, :xps_12, :xps_13, :xps_15, :inspiron_desktop, :inspiron_aio, :monitor_item, :promotion_item]
   mount_uploader :file_name, FileNameUploader
   validates :file_name, presence: true
 end
