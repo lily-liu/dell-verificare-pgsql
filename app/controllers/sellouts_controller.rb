cf913497c3eb671a7f2bd4f78e4a61ef8f976edb
wisely <wisely.dennis@student.sgu.ac.id>
2016-12-16 13:27:56 +0700

    fix sellout cant update

--- app/controllers/sellouts_controller.rb
+++ app/controllers/sellouts_controller.rb
@@ -1,5 +1,5 @@
 class SelloutsController < ApplicationController
-  # before_action :set_sellout, only: [:show, :update, :destroy]
+  before_action :set_sellout, only: [:show, :update, :destroy]
   before_action :authenticate_user
 
 
