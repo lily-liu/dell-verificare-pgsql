c60029a492d05c18d7278ce48280367d33634563
wisely <wisely.dennis@student.sgu.ac.id>
2017-02-24 13:48:56 +0700

    fixed absence report

--- app/builders/absence_builder.rb
+++ app/builders/absence_builder.rb
@@ -1,7 +1,8 @@
 class AbsenceBuilder < Julia::Builder
+  asd = :created_at
   column 'Username', :username
   column 'Store Name', :name
   column 'Absence Type', :absence_type
-  column 'Remark', :remark
-  column 'Absence Time', :created_at
+  column 'Absence Date', -> { "#{created_at.to_date}" }
+  column 'Absence Time', -> { "#{created_at.to_time.getlocal("+07:00")}" }
 end
