327c17024d4be1242ff0bf2c71c7a83e83b1af4d
wisely <wisely.dennis@student.sgu.ac.id>
2017-03-15 11:37:16 +0700

    fix photo blur on upload

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -26,7 +26,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [300, 300]
+  # process :resize_to_fit => [300, 300]
 
   # def scale(width, height)
   #   # do something
