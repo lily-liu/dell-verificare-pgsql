1e02e7fd6d008329ed7ba2011de567479a419973
wisely <wisely.dennis@student.sgu.ac.id>
2017-03-22 17:25:57 +0700

    fixed inventories graph missing

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -126,13 +126,13 @@ class InventoriesController < ApplicationController
   end
 
   def inventories_per_cam
-    @report_unsorted = Manager.select(:name).joins(users: :inventories).where('inventories.status': 0).group(:name).count
+    @report_unsorted = Manager.select(:name).joins(users: :inventories).where('inventories.status': 1).group(:name).count
     @report = @report_unsorted.sort_by { |k, v| v }.reverse.to_h
     render :report, status: :ok
   end
 
   def inventories_each_cam_per_store
-    @report_unsorted = Store.select(:name).joins(inventories: [{user: :manager}]).joins(:inventories).where(managers: {id: params.fetch(:manager_id).to_i}).where('inventories.status': 0).group(:name).count
+    @report_unsorted = Store.select(:name).joins(inventories: [{user: :manager}]).joins(:inventories).where(managers: {id: params.fetch(:manager_id).to_i}).where('inventories.status': 1).group(:name).count
     @report = @report_unsorted.sort_by { |k, v| v }.reverse.to_h
     render :report, status: :ok
   end
