d5de2923b67d1652e92c018ac43bf6968328f3a2
lily-liu <wisely.dennis@student.sgu.ac.id>
2019-03-13 22:59:47 +0700

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
       provider: 'AWS', # required
-      aws_access_key_id: 'AKIAJ3B6D57O2IGVIGAQ', # required
-      aws_secret_access_key: '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', # required
-      region: 'ap-southeast-1', # optional
+      aws_access_key_id: 'asdasdasdasd', # required
+      aws_secret_access_key: 'asdasdasdasdasdasdasdasdasd', # required
+      region: 'asdsad-asdasdsad-asdsad', # optional
   }
   config.fog_directory = 'verificareuploads1' # required
 end
